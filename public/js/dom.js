606e694e7c0e25f620000a7e7f3d5df49ff0ce98
Fernando L. Canizo <flc@muriandre.com>
2015-06-12 09:29:59 -0300

    DOM helper module to manage CSS classes

---
+++ public/js/dom.js
@@ -0,0 +1,38 @@
+"use strict";
+/* global Sizzle */
+
+
+// TODO remove this when I get Modernizr
+var bejerked = {};
+
+bejerked.dom = (function () {
+	var $ = Sizzle;
+
+	function hasClass(element, className) {
+		var regex = new RegExp("(^|\\s)" + className + "(\\s|$)");
+
+		return regex.test(element.className);
+	}
+
+
+	function addClass(element, className) {
+		if(! hasClass(element, className)) {
+			element.className += " " + className;
+		}
+	}
+
+
+	function removeClass(element, className) {
+		var regex = new RegExp("(^|\\s)" + className + "(\\s|$)");
+
+		element.className.replace(regex, " ");
+	}
+
+
+	return {
+		$: $,
+		hasClass: hasClass,
+		addClass: addClass,
+		removeClass: removeClass
+	};
+}());
